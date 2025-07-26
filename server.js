5742bd86e5642b7461c42d225b827bebd5da3833
Miaad Asjad <miaadasjad1514@gmail.com>
2025-07-26 16:50:19 +0500

    small changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/contact", contactRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
